06511d1801842e0b653fcfa656c9ded07225a44e
Jorge221z <jorgemunozcast12@gmail.com>
2025-04-03 13:14:05 +0200

    Middleware for candidates and companies. Implemented to bootstrap/app.php

--- EmpleaWorks/bootstrap/app.php
+++ EmpleaWorks/bootstrap/app.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use App\Http\Middleware\EnsureCompanyRole;
+use App\Http\Middleware\EnsureCandidateRole;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,7 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+            EnsureCandidateRole::class,
+            EnsureCompanyRole::class,
         ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
