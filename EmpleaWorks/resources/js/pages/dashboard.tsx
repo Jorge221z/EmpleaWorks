91738be44f5147d0d4d050ae9d7b6b2544e7ce81
Jorge221z <jorgemunozcast12@gmail.com>
2025-04-07 10:54:11 +0200

    Modular types implementation

--- EmpleaWorks/resources/js/pages/dashboard.tsx
+++ EmpleaWorks/resources/js/pages/dashboard.tsx
@@ -4,30 +4,9 @@ import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem, type SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
 import { CalendarIcon, MapPinIcon, BriefcaseIcon } from 'lucide-react';
+import { Offer, Company } from '@/types/types';
 
-interface Company {
-    id: number;
-    created_at: string;
-    updated_at: string;
-    name: string;
-    logo: string | null;
-    email: string;
-    address: string | null;
-    description: string | null;
-    web_link: string | null;
-  }
 
-interface Offer {
-    id: number;
-    name: string;
-    description: string;
-    category: string;
-    degree: string;
-    contract_type: string;
-    job_location: string;
-    closing_date: string;
-    company: Company;
-  }
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
