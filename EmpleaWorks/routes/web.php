3d575c3e9587855adde97ca48102d12eb8343f07
Adrii-Dev <ibaez6adri@gmail.com>
2025-04-07 10:50:54 +0200

    Fixed web.php routes

--- EmpleaWorks/routes/web.php
+++ EmpleaWorks/routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\DashboardController;
 // Home y Dashboard (mostrar ofertas)
 Route::get('/', [DashboardController::class, 'index'])->name('home');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/offers/{offer}', [DashboardController::class, 'showOffer'])->name('offers.show');
 
 // Fallback routes in case auth.php routes aren't working properly
 Route::get('/login-direct', [AuthenticatedSessionController::class, 'create'])
@@ -22,8 +23,5 @@ require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
 
 // Offer routes
-// API para obtener ofertas (solo datos)
+// Test de API para obtener ofertas (solo datos)
 Route::get('/offers', [OfferController::class, 'list'])->name('offers.list');
-
-// Mostrar oferta específica
-Route::get('/offers/{offer}', [DashboardController::class, 'showOffer'])->name('offers.show');
