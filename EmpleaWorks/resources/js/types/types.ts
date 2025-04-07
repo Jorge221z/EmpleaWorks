edc9c4ccfe2d157c3860af9b65df35edd45641f6
Jorge221z <jorgemunozcast12@gmail.com>
2025-04-07 11:01:44 +0200

    types file needed to modular code

---
+++ EmpleaWorks/resources/js/types/types.ts
@@ -0,0 +1,29 @@
+//definimos aqui los tipos de array de objetos para ganar modularidad y reutilizar el código//
+
+export interface Offer {
+  id: number;
+  name: string;
+  description: string;
+  category: string;
+  degree: string;
+  email: string;
+  contract_type: string;
+  job_location: string;
+  closing_date: string;
+  company_id: number;
+  created_at: string;
+  updated_at: string;
+  company: Company;
+}
+
+export interface Company {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  name: string;
+  logo: string;
+  email: string;
+  address: string;
+  description: string;
+  web_link: string;
+}
